9b20aa8966c049821f2f67b80f24f678989c1894
Foodie <olaorefouad565@gmail.com>
2020-07-30 22:49:26 +0100

    feature: worked on the home screen, inflated the chips into the chip group and loaded the cuisine data into them

--- app/src/main/java/dev/olaore/recipeze/fragments/HomeFragment.kt
+++ app/src/main/java/dev/olaore/recipeze/fragments/HomeFragment.kt
@@ -1,18 +1,23 @@
 package dev.olaore.recipeze.fragments
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.chip.Chip
 
 import dev.olaore.recipeze.R
 import dev.olaore.recipeze.databinding.FragmentHomeBinding
 import dev.olaore.recipeze.obtainViewModel
 import dev.olaore.recipeze.viewmodels.HomeViewModel
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,6 +26,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var viewModel: HomeViewModel
+    private lateinit var cuisines: Array<String>
 
     private val TAG = "HomeFragment"
 
@@ -40,13 +46,43 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.user.observe(viewLifecycleOwner, Observer {
-            Log.d(TAG, "$it")
+            cuisines = it.cuisines.split(",").toTypedArray()
+            setUpChips()
         })
 
         viewModel.randomRecipes.observe(viewLifecycleOwner, Observer {
             Log.d(TAG, "Here?")
             Log.d(TAG, "Recipes Size: ${ it.recipes.size }")
         })
+
+    }
+
+    private fun setUpChips() {
+        recipes_chip_group.isSingleSelection = true
+        val allChip = layoutInflater.inflate(R.layout.single_chip_layout, recipes_chip_group, false) as Chip
+        allChip.text = resources.getString(R.string.all_text)
+        allChip.isCheckable = true
+        allChip.id = 0
+        allChip.isClickable = true
+
+        recipes_chip_group.addView(allChip)
+
+        cuisines.forEachIndexed { index, it ->
+            val chip = layoutInflater.inflate(R.layout.single_chip_layout, recipes_chip_group, false) as Chip
+            chip.text = it
+            chip.isCheckable = true
+            chip.isClickable = true
+            chip.id = index + 1
+            recipes_chip_group.addView(chip)
+        }
+
+        recipes_chip_group.setOnCheckedChangeListener { _, checkedId ->
+            if (checkedId == 0) {
+                Toast.makeText(requireContext(), "You just clicked All", Toast.LENGTH_LONG).show()
+                return@setOnCheckedChangeListener
+            }
+            Toast.makeText(requireContext(), "You just clicked ${ cuisines[checkedId - 1] }", Toast.LENGTH_LONG).show()
+        }
 
     }
 
