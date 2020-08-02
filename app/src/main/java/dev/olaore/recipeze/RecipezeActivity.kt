373e6dc428fb58cd261571c1e276bb2401449df8
Foodie <olaorefouad565@gmail.com>
2020-08-02 21:02:27 +0100

    feature: started working on the custom toolbar and porting to a diff layout file

--- app/src/main/java/dev/olaore/recipeze/RecipezeActivity.kt
+++ app/src/main/java/dev/olaore/recipeze/RecipezeActivity.kt
@@ -2,11 +2,16 @@ package dev.olaore.recipeze
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_recipeze.*
 
 class RecipezeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipeze)
+
+        setSupportActionBar(recipeze_toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        title = ""
     }
 }
