8785d92cc49fd13aac4d720b4328060d340692b0
Foodie <olaorefouad565@gmail.com>
2020-08-05 22:47:15 +0100

    feature: added the custom toolbar and configured the navigation view and it's menu, but it crashes upon startup

--- app/src/main/java/dev/olaore/recipeze/RecipezeActivity.kt
+++ app/src/main/java/dev/olaore/recipeze/RecipezeActivity.kt
@@ -2,9 +2,17 @@ package dev.olaore.recipeze
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_recipeze.*
+import kotlinx.android.synthetic.main.app_bar.*
 
 class RecipezeActivity : AppCompatActivity() {
+
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +21,21 @@ class RecipezeActivity : AppCompatActivity() {
         setSupportActionBar(recipeze_toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         title = ""
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.recipeze_navigation_view)
+
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, recipeze_toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener {
+            val id = it.itemId
+
+            // make selection
+
+            false
+        }
+
     }
 }
