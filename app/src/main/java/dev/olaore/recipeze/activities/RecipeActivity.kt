a079d0e1165b4d32453c0f692dedf2896d3d2735
Foodie <olaorefouad565@gmail.com>
2020-11-18 22:13:45 +0100

    feature: added the persistent bottom sheet but a rendering error occurred and the bottom sheet does not support dragging

--- app/src/main/java/dev/olaore/recipeze/activities/RecipeActivity.kt
+++ app/src/main/java/dev/olaore/recipeze/activities/RecipeActivity.kt
@@ -2,12 +2,21 @@ package dev.olaore.recipeze.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.widget.NestedScrollView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dev.olaore.recipeze.R
 
 class RecipeActivity : AppCompatActivity() {
 
+    lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
+    lateinit var bottomSheet: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe)
+
+        bottomSheet = findViewById(R.id.recipe_bottom_sheet)
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
     }
 }
