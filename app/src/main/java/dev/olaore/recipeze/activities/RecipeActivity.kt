a021aceb4132cccf4aeb0fd0463be923ef44025c
Foodie <olaorefouad565@gmail.com>
2020-11-23 20:54:49 +0100

    fix: worked on the false rendering of bottom sheet, set it to peek at half the screen and it's full height to the height of the screen, enabled dragging and expansion but disabled hiding so it's always on-screen

--- app/src/main/java/dev/olaore/recipeze/activities/RecipeActivity.kt
+++ app/src/main/java/dev/olaore/recipeze/activities/RecipeActivity.kt
@@ -1,8 +1,11 @@
 package dev.olaore.recipeze.activities
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import androidx.core.widget.NestedScrollView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dev.olaore.recipeze.R
@@ -18,5 +21,26 @@ class RecipeActivity : AppCompatActivity() {
 
         bottomSheet = findViewById(R.id.recipe_bottom_sheet)
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+
+        val layoutParams = bottomSheet.layoutParams
+        layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
+        bottomSheet.layoutParams = layoutParams
+
+        bottomSheetBehavior.peekHeight = Resources.getSystem().displayMetrics.heightPixels / 2
+
+        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> Log.d("RecipeActivity", "Collapsed")
+                    BottomSheetBehavior.STATE_DRAGGING -> Log.d("RecipeActivity", "Dragging")
+                    BottomSheetBehavior.STATE_EXPANDED -> Log.d("RecipeActivity", "Expanded")
+                    BottomSheetBehavior.STATE_SETTLING -> Log.d("RecipeActivity", "Settling")
+                }
+            }
+        })
     }
 }
